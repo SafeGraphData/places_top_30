98b9f458692faffad3ba9aaa7baf486a79d720c0
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-17 11:39:34 -0500

    Create read_data.py

---
+++ read_data.py
@@ -0,0 +1,22 @@
+import gspread
+import streamlit as st
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+def read_from_gsheets(sheet_name):
+    scope = ["https://www.googleapis.com/auth/spreadsheets"]
+    credentials_dict = st.secrets["gcp_service_account"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
+
+    gc = gspread.authorize(credentials)
+    sheet = gc.open_by_url(st.secrets["private_gsheets_url"])
+
+    worksheet = sheet.worksheet(sheet_name)
+
+    data = worksheet.get_all_values()
+
+    headers = data[0]
+    rows = data[1:]
+
+    df = pd.DataFrame(rows, columns=headers)
+    return df
